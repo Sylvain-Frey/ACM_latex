a7f0f23eaa9c7970e9f6934a37f81bbe2f900063
Sylvain-Frey <s.frey@lancaster.ac.uk>
2015-01-27 15:56:34 +0000

    Added continuous compilation

--- compile.sh
+++ compile.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
+
+doc_name="paper"
 export BIBINPUTS=.
 
 if [ $1 = "c" ]
 then
     # continuous compilation
-    echo "# Waiting for changes in source files... (in *.tex and  sections/*.tex)"
+    echo "# Waiting for changes in source files... (in *.tex and sections/*.tex)"
     while true; do 
         inotifywait -e modify *.tex sections/*.tex
-        pdflatex --output-directory=build -interaction=nonstopmode paper.tex
-        mv build/paper.pdf .
+        pdflatex --output-directory=build -interaction=nonstopmode $doc_name.tex
+        mv build/$doc_name.pdf .
     done
 else
     # full build
     rm -rf build/*
-    latex --output-directory=build -interaction=nonstopmode paper.tex
-    bibtex build/paper
-    latex --output-directory=build -interaction=nonstopmode paper.tex
-    pdflatex --output-directory=build -interaction=nonstopmode paper.tex
-    mv build/paper.pdf .
+    latex --output-directory=build -interaction=nonstopmode $doc_name.tex
+    bibtex build/$doc_name
+    latex --output-directory=build -interaction=nonstopmode $doc_name.tex
+    pdflatex --output-directory=build -interaction=nonstopmode $doc_name.tex
+    mv build/$doc_name.pdf .
 fi
